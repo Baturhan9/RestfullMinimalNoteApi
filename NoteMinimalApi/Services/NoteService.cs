a5cfae010b7bd574b22dc53ba6c13ff8e3c2d9f2
Baturhan <baturxan-06.72@mail.ru>
2024-04-03 17:49:18 +0600

    Add note and user services

--- NoteMinimalApi/Services/NoteService.cs
+++ NoteMinimalApi/Services/NoteService.cs
@@ -1,6 +1,73 @@
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using NoteMinimalApi.Models;
+using NoteMinimalApi.ViewModels.NoteViewModel;
+
 namespace NoteMinimalApi.Services;
 
 public class NoteService
 {
-     
+    private AppDbContext _context;
+    public NoteService(AppDbContext context)
+    {
+        _context = context; 
+    }
+
+    public async Task<int> CreateNoteAsync(CreateNoteCommand cmd)
+    {
+        var note = cmd.ToNote();
+        _context.Notes.Add(note);
+        await _context.SaveChangesAsync();
+        return note.NoteId;
+    }
+
+    public async Task<List<NoteSummaryViewModel>> GetNotesAsync()
+    {
+        return await _context.Notes.Where(n => !n.IsDeleted).Select(n => new NoteSummaryViewModel
+        {
+            NoteId = n.NoteId,
+            Title = n.Title,
+            DateModified = n.DateModified ?? n.DateCreated
+        }).ToListAsync();
+    }
+
+    public async Task<NoteDetailViewModel?> GetNoteDetailAsync(int id)
+    {
+        return await _context.Notes.Where(s => !s.IsDeleted && s.NoteId == id)
+            .Select(n => new NoteDetailViewModel
+            {
+                NoteId = n.NoteId,
+                Title = n.Title,
+                Description = n.Description,
+                DateCreated = n.DateCreated,
+                DateModified = n.DateModified
+            }).SingleOrDefaultAsync();
+    }
+
+    public async Task UpdateNoteAsync(UpdateNoteCommand cmd)
+    {
+        var note = await _context.Notes.FindAsync(cmd.NoteId);
+        if(note is null)
+            throw new Exception("Unable to find the note");
+        if(note.IsDeleted)
+            throw new Exception("Note deleted");
+
+        note.Title = cmd.Title;
+        note.Description = cmd.Description;
+
+        await _context.SaveChangesAsync();
+    }
+
+
+    public async Task DeleteNoteASync(int id)
+    {
+        var note = await _context.Notes.FindAsync(id);
+        if(note is null)
+            throw new Exception("Unable to find the note");
+
+        note.IsDeleted = true;
+
+        await _context.SaveChangesAsync();
+    }
+
 }
---
+++ NoteMinimalApi/Services/UserService.cs
@@ -0,0 +1,75 @@
+using Microsoft.EntityFrameworkCore;
+using NoteMinimalApi.Models;
+using NoteMinimalApi.ViewModels.UserViewModel;
+
+namespace NoteMinimalApi.Services;
+
+public class UserService
+{
+    private AppDbContext _context;
+
+    public UserService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> CreateUserAsync(CreateUserCommand cmd)
+    {
+        var user = cmd.ToUser();
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return user.UserId;
+    }
+
+    public async Task<List<UserSummaryViewModel>> GetUsersAsync()
+    {
+        return await _context.Users.Where(u => !u.IsDeleted)
+            .Select(u => new UserSummaryViewModel
+            {
+                UserId = u.UserId,
+                UserName = $"{u.FirstName} {u.LastName} aka {u.Login}"
+            })
+            .ToListAsync();
+    }
+
+    public async Task<UserDetailViewModel?> GetUserDetailAsync(int id)
+    {
+        return await _context.Users.Where(u => u.UserId == id && !u.IsDeleted)
+            .Select(u => new UserDetailViewModel
+            {
+                UserId = u.UserId,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                Login = u.Login,
+                Password = u.Password
+            })
+            .SingleOrDefaultAsync();
+    }
+
+    public async Task UpdateUserAsync(UpdateUserCommand cmd)
+    {
+        var user = await _context.Users.FindAsync(cmd.UserId);
+        if(user is null)
+            throw new Exception("Unable to find the user");
+        if(user.IsDeleted)
+            throw new Exception("User deleted");
+
+        user.FirstName = cmd.FirstName;
+        user.LastName = cmd.LastName;
+        user.Login = cmd.Login;
+        user.Password = cmd.Password;
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteUserAsync(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if(user is null)
+            throw new Exception("Unable to find the user");
+        
+        user.IsDeleted = true;
+        await _context.SaveChangesAsync();
+    }
+
+}
